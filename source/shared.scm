d5e3aca51c7adb9d69554ca753928eb14ef049db
Dmitry Dzhus <mail@sphinx.net.ru>
2007-10-24 18:13:13 +0400

    Explicit whitespace in TeX math comments.

--- source/shared.scm
+++ source/shared.scm
@@ -7,38 +7,36 @@
                              mult add zero)
   (fold-right 
    (lambda (this-coeff higher-terms)
-     (add 
-      (mult higher-terms x)
-      this-coeff))
+     (add (mult higher-terms x) this-coeff))
    zero
    coefficient-sequence))
 
-;;@ $l, l+1 \ldots h-1, h$
+;;@ $l,\ l+1\ \ldots\ h-1,\ h$
 (define (enumerate-interval low high)
   (if (> low high)
-      (list)
+      '()
       (cons low (enumerate-interval (+ low 1) high))))
 
-;;@ $ 1, 2 \ldots n$
+;;@ $ 1,\ 2\ \ldots\ n$
 (define (enumerate-n n)
   (enumerate-interval 1 n))
 
-;;@ $a_{s_1}, a_{s_2} \ldots a_{s_n}, a_{s_k} = f(a_{s_{k-1}}, s_k)$
+;;@ $a_{s_1},\ a_{s_2}\ \ldots\ a_{s_n},\ a_{s_k} = f(a_{s_{k-1}}, s_k)$
 (define (evolve-sequence evolve initial index)
-  (define (iter index prev result)
+  (define (evolve-next index prev result)
     (if (null? index)
         result
         (let ((current (evolve prev (car index))))
-          (iter (cdr index)
+          (evolve-next (cdr index)
                 current
                 (append result (list current))))))
-  (iter index initial (list initial)))
+  (evolve-next index initial (list initial)))
 
-;;@ $a_1, a_2 \ldots a_n$
+;;@ $a_1,\ a_2\ \ldots\ a_n,\ a_k=f(a_{k-1})$
 (define (evolve-series evolve initial n)
   (evolve-sequence evolve initial (enumerate-n n)))
 
-;;@ $ 1/0!,\ 1/1!,\ 1/2! \ldots 1/n!$
+;;@ $ 1/0!,\ 1/1!,\ 1/2! \ldots 1/(n-1)!$
 (define (exp-series-coefficients n)
   (evolve-series (lambda (prev i) (/ prev i)) 
                  1
