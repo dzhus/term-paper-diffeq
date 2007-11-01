25414d644bacdfc95739a87df8a20f59edc26eac
Dmitry Dzhus <mail@sphinx.net.ru>
2007-11-01 19:24:15 +0300

    `texify-results.sh` now really respects energy conservation check result.
    Plot inclusion commands moved back to `results.tpl.tex`.

--- doc/texify-results.sh
+++ doc/texify-results.sh
@@ -11,7 +11,8 @@ TEMPFILE=$(mktemp /tmp/docXXXXXX)
 
 RESULTS=$1
 
-cat ${RESULTS} | tail -n +$(($(grep -n "%%" ${RESULTS} | sed -e "s/:.*//")+1)) > ${TEMPFILE}
+cat ${RESULTS} | tail -n +$(($(grep -n "%%" ${RESULTS} | sed -e "s/:.*//")+1)) \
+    > ${TEMPFILE}
 
 # DRY violation follows
 
@@ -19,9 +20,11 @@ A=$(grep "A:" ${TEMPFILE} | sed -e "s/.*: //")
 B=$(grep "B:" ${TEMPFILE} | sed -e "s/.*: //")
 EPS=$(grep "eps:" ${TEMPFILE} | sed -e "s/.*: //")
 METHOD=$(grep "method: " ${TEMPFILE} | sed -e "s/.*: //")
+SUCCESS="__CONSERVE_"$(grep "conserves: " ${TEMPFILE} | sed -e "s/.*: //")
 
 m4 --define="__A"="${A}" \
     --define="__B"="${B}" \
     --define="__EPS"="${EPS}" \
     --define="__METHOD"="${METHOD}" \
+    --define="__CONSERVE_STATUS"="${SUCCESS}" \
     results.tpl.tex
