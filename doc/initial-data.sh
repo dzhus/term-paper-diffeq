f17b685c7a8f91661ad1cc2ec9d9f3743f9528e2
Dmitry Dzhus <mail@sphinx.net.ru>
2007-11-04 14:39:51 +0300

    Initial data included in report from file `statement.scm` by command `\input{statement.scm-initial-data}` may now be referenced with `\eqref{statement.scm}`

--- doc/initial-data.sh
+++ doc/initial-data.sh
@@ -14,7 +14,7 @@ TEMPFILE=$(mktemp /tmp/docXXXXXX)
 
 # Strip out non-TeXish comments
 
-cat $1 | grep -v "[ ]*;; .*" > ${TEMPFILE}
+cat ${1} | grep -v "[ ]*;; .*" > ${TEMPFILE}
 
 # Extract numeric parameters
 
@@ -24,6 +24,9 @@ RIGHTBOUND=$(cat ${TEMPFILE} | grep right-bound | \
 NTEX=$(cat ${TEMPFILE} | grep ';;@ \$n(x)' | \
     sed -e "s/;;@ //" | sed -e 's/^\$\(.*\)\$$/\1/g' | tr -d "\n")
 
+LABEL=$(basename ${1})
+
 m4 --define="__NTEX"="${NTEX}" \
     --define="__RIGHTBOUND"="${RIGHTBOUND}" \
+    --define="__LABEL"="${LABEL}" \
     initial-data.tpl.tex
