d6c9d363a15fa28ae625b6f646f2602f7bff9913
Dmitry Dzhus <mail@sphinx.net.ru>
2007-11-05 00:55:30 +0300

    Decomposed `green-transform` into two functions. `green-subtransform` is now used both by `green-transform` and `find-A-B` to create integrand for B coefficient expression.

--- source/iterative-solution.scm
+++ source/iterative-solution.scm
@@ -1,12 +1,17 @@
 (load "shared.scm")
 
-;;@ $\varphi \colon n(x), u(x) \rightarrow f(x) = e^{ik\abs{x-y}}(k^2-n(y))u(y)$
-(define (green-transform u n)
+;;@ $\varphi (n(x), u(x), g(x, y)) = f(x,y) = e^{ik \cdot g(x, y)}(k^2-n(y))u(y)$
+(define (green-subtransform u n g)
   (let ((k (get-wave-number n)))
     (lambda (x y)
-      (* (exp (* +i k (abs (- x y))))
+      (* (exp (* +i k (g x y)))
          (- (sqr k) (n y))
          (u y)))))
+
+;;@ $\hat{\varphi} (n(x), u(x), g(x, y) ) = f(x,y) = e^{ik \abs{(x-y}}(k^2-n(y))u(y)$
+(define (green-transform u n)
+  (green-subtransform u n
+                      (lambda (x y) (abs (- x y)))))
 
 ;; Simpson's formulæ for functions of two arguments
 ;;@ $\int_a^b f(x) dx$
@@ -26,19 +31,24 @@
             (f x b))))))
 
 ;;@ $\op{A} \colon \frac{1}{2ik} \int_0^a {e^{ik|x-y|}(k^2-n(y))u(y) dy} \circ u(x)$
-(define (green-integrate u n a subintervals)
-  (let ((k (get-wave-number n)))
+(define (green-integrate u refraction right-bound subintervals)
+  (let ((k (get-wave-number refraction)))
     (lambda (x)
-      (/ ((integrate (green-transform u n) 0 a (/ subintervals 2)) x)
+      (/ ((integrate (green-transform u refraction) 
+                     0 right-bound (/ subintervals 2)) x)
          (* 2 +i k)))))
 
 ;; Find A, B, given solution u(x), n(x), [0;a] and subintervals count
 (define (find-A-B solution refraction right-bound subintervals)
   (let ((k (get-wave-number refraction)))
-    (let ((A ((green-integrate solution refraction right-bound subintervals) 0))
-          (B (+ (/ ((integrate (lambda (x y) (* (exp (* -i k y))
-                                                (- (sqr k) (refraction y))
-                                                (solution y)))
+    (let (
+          ;;@ $A = \frac{1}{2ik}\int_0^a{e^{iky}(k^2-n(y))u(y)dy}$
+          (A ((green-integrate solution refraction
+                               right-bound subintervals) 0))
+          ;;@ $B = \frac{1}{2ik}\int_0^a{e^{-iky}(k^2-n(y))u(y)dy} + 1$
+          (B (+ (/ ((integrate (green-subtransform
+                                solution refraction 
+                                (lambda (x y) (- y)))
                                0 right-bound subintervals) 0)
                    (* 2 +i k))
                 1)))
@@ -46,9 +56,9 @@
 
 ;; Keep applying `green-integrate` to initial approximation until A
 ;; and B meet the law of energy conservation.
-;;
+;; 
 ;; Returns a pair (U . COEFFS), where U is an approximate solution
-;;_function_ and COEFFS is (A . B) pair
+;; _function_ and COEFFS is (A . B) pair
 (define (make-solution refraction right-bound subintervals eps)
   (let* ((k (get-wave-number refraction))
          (initial-solution (cons (wave k)
