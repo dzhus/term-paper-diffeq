4245eee391ce32f236a5c5dba35e0e9da2e8b915
Dmitry Dzhus <mail@sphinx.net.ru>
2007-11-08 18:51:16 +0300

    Using semantic `\dotsc` in `shared.scm` TeX-comments instead of `\ldots`.

--- source/shared.scm
+++ source/shared.scm
@@ -5,9 +5,10 @@
   (fold + 0 sequence))
 
 ;;@ $x^2$
-(define (sqr x) (* x x))
+(define (sqr x)
+  (* x x))
 
-;;@ $( \ldots ((a_n \cdot x + a_{n-1}) \cdot x + a_{n-2}) \cdot x \ldots + a_1) \cdot x + a_0)$
+;;@ $( \dotsb ((a_n \cdot x + a_{n-1}) \cdot x + a_{n-2}) \cdot x \dotsb + a_1) \cdot x + a_0)$
 (define (general-horner-eval x coefficient-sequence
                              mult add zero)
   (fold-right 
@@ -16,7 +17,7 @@
    zero
    coefficient-sequence))
 
-;;@ $l, l+1 \ldots h-1,\ h$
+;;@ $l, l+1, \dotsc, h-1,\ h$
 (define (enumerate-interval low high)
   (define (iter low high acc)
     (if (> low high)
@@ -24,11 +25,11 @@
         (iter low (- high 1) (cons high acc))))
   (iter low high '()))
 
-;;@ $ 1, 2 \ldots n$
+;;@ $ 1, 2, \dotsc, n$
 (define (enumerate-n n)
   (enumerate-interval 1 n))
 
-;;@ $a_{s_1}, a_{s_2} \ldots a_{s_n},\ a_{s_k} = f(a_{s_{k-1}}, s_k)$
+;;@ $a_{s_1}, a_{s_2}, \dotsc, a_{s_n},\ a_{s_k} = f(a_{s_{k-1}}, s_k)$
 (define (evolve-sequence evolve initial index)
   (define (evolve-next index prev acc)
     (if (null? index)
@@ -39,18 +40,18 @@
                 (append acc (list current))))))
   (evolve-next index initial (list initial)))
 
-;;@ $a_1, a_2 \ldots a_n,\ a_k=f(a_{k-1})$
+;;@ $a_1, a_2, \dotsc, a_n,\ a_k=f(a_{k-1})$
 (define (evolve-series evolve initial n)
   (evolve-sequence evolve initial (enumerate-n n)))
 
-;;@ $x_1, x_2 \ldots x_{n-1},\ x_k = a+h(k-\frac{1}{2}), h = (b-a)/n$
+;;@ $x_1, x_2, \dotsc, x_{n-1},\ x_k = a+h(k-\frac{1}{2}), h = (b-a)/n$
 (define (split-interval a b n)
   (let ((step (/ (- b a) n)))
     (evolve-series (lambda (x n) (+ x step)) 
                    (+ a (/ step 2))
                    (- n 1))))
 
-;;@ $ 1/0!, 1/1!, 1/2! \ldots 1/(n-1)!$
+;;@ $ 1/0!, 1/1!, 1/2!, \dotsc, 1/(n-1)!$
 (define (exp-series-coefficients n)
   (evolve-series (lambda (prev i) (/ prev i)) 
                  1
@@ -66,6 +67,7 @@
      eps))
 
 ;; Calculate a set of f(x) values on given [a; b] interval
+;;@ $f(x_1), f(x_2), \dotsc, f(x_{n-1}),\ x_k = a+h(k-\frac{1}{2}), h = (b-a)/n$
 (define (tabulate-function f a b subintervals)
   (map f (split-interval a b subintervals)))
 
